f9c11bbcee884d18f074cffe4a2accdbf53f30f6
Mahendra Rai <mahemrai@googlemail.com>
2014-02-22 22:32:28 +0000

    initial commit

---
+++ public/index.php
@@ -0,0 +1,28 @@
+<?php
+require '../vendor/autoload.php';
+
+//Slim app configuration
+$app = new \Slim\Slim(array(
+	'templates.path' => '../app/views',
+	'view' => new \Slim\Views\Twig()
+));
+
+
+//View configuration
+$view = $app->view();
+$view->parserOptions = array(
+	'charset' => 'utf-8',
+	'cache' => realpath('../app/views/cache'),
+	'auto_reload' => true,
+	'strict_variables' => false,
+	'autoescape' => true
+);
+
+$view->parserExtensions = array(new \Slim\Views\TwigExtension());
+
+$app->get('/', function() use ($app) {
+	$app->render('test.html');
+});
+
+$app->run();
+?>
